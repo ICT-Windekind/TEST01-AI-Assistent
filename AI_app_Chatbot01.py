39ad85949b277c51f3a3eb5e808a1ca3d66feca2
ICT-Windekind <ict@windekindleuven.be>
2026-02-04 10:35:29 +0000

    chatbot

--- AI_app_Chatbot01.py
+++ AI_app_Chatbot01.py
@@ -7,17 +7,17 @@ st.markdown("Joost Delie - 2 februari 2026")
 form = st.form(key="user_settings")
 with form:
     DOEL_concept = st.text_input("Welk doel wil je omzetten naar een SMARTi doel:", key = "DOEL_concept")
-    role = st.selectbox("Voor welk publiek wil je het uitgelegd hebben?",
-                          ("Expert",
-                           "Leek", "ouder"))
+    #role = st.selectbox("Voor welk publiek wil je het uitgelegd hebben?",
+                          #("Expert",
+                           #"Leek", "ouder"))
     niveau = st.selectbox("Over welke leerling gaat het",("kleuter onderwijs","Lager onderwijs","Secundair onderwijs"))
     generate_button = form.form_submit_button("Zet je doel om")
     if generate_button:
         with st.spinner('Even geduld...'):
             PROMPT1 = f"""Verwoord je doel: {DOEL_concept} , aan de hand van volgende site 
             https://pro.katholiekonderwijs.vlaanderen/kwaliteitsinstrumenten/smartidoelen-stellen 
-            zodat het duidelijk leesbaar is voor een {role} op het gebied van leerlingen ondersteunen.
-            Voeg ook 1 concrete actie toe die hierbij past om met een leerling van het {niveau} aan de slag te gaan. 
+            zodat het duidelijk leesbaar is voor iedereen die betrokken is bij het ondersteunen van leerlingen in het {niveau}.
+            Voeg ook 1 concrete actie toe die hierbij past om met die leerling aan de slag te gaan. 
             Antwoord in het Nederlands."""
             response1 = chatbot_response(PROMPT1);
         st.write(response1)
